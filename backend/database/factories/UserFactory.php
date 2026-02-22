82661fed055c844a7c65117ab8a3c6f99232b5f7
el-pablos <yeteprem.end23juni@gmail.com>
2026-02-22 08:24:45 +0700

    fix: publish migration sanctum & fix factory user level jadi guest

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -29,6 +29,10 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'phone' => fake()->phoneNumber(),
+            'role' => 'user',
+            'member_level' => 'guest',
+            'is_active' => true,
         ];
     }
 
